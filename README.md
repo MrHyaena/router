76a9ac916cdb7fdcb13842a906d4d89c2f301813
MrHyaena <dolezalmartin131@gmail.com>
2024-11-20 16:20:46 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# React template
+
+<h1>What does it contain</h1>
+
+- React
+- NPM
+- Vite
+- Vitest
+- Prop-types
---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+import "./App.css";
+
+const App = (props) => {
+  return <h1>Hello</h1>;
+};
+
+export default App;
